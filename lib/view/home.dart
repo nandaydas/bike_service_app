c381c0e79e01c54273d39815d077a24cebe9f712
Pritam Das <daspritam4231@gmail.com>
2022-07-23 23:06:34 +0530

    committed

---
+++ lib/view/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hedarfut extends StatelessWidget {
+  const Hedarfut({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
