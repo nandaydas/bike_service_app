d22ee5a6aa691022265ca2b1a9db9d3bbde34f1b
Pritam Das <daspritam4231@gmail.com>
2022-08-01 23:56:03 +0530

    Account Data Fetching updated

--- lib/view/User_side/user_dashboard.dart
+++ lib/view/User_side/user_dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:bike_service_app/controller/account_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import '../account_page.dart';
 import '../history_page.dart';
 import 's_centers_page.dart';
@@ -9,12 +11,15 @@ class UserDashboard extends StatefulWidget {
 }
 
 class _MyDashboardState extends State<UserDashboard> {
+  AccountController ac = Get.find<AccountController>();
+
   int _currentTab = 0;
   List _Body = [ServicesPage(), HistoryPage(), AccountPage()];
   List _Title = ['Service Centers', 'Service History', 'My Account'];
 
   @override
   Widget build(BuildContext context) {
+    ac.fetchData();
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFA),
       appBar: AppBar(
